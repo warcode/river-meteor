2c5e3e8292c5e39a0ca411bc545f130c61164f3b
warcode <warcode@bang>
2015-06-10 20:49:38 +0200

    Ported basic river layout

--- river-meteor.js
+++ river-meteor.js
@@ -1,5 +1,8 @@
+Tweets = new Mongo.Collection("tweets");
+
 if (Meteor.isClient) {
   // counter starts at 0
+  /*
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -14,10 +17,32 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+  */
+  
+	Template.body.helpers({
+		tweets: function () {
+			return Tweets.find({}, {sort: {created_at: -1}});
+		}
+	}); 
+  
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Tweets.remove({});
+    Tweets.insert({
+		river_user: 'warcode', 
+		twitter_data : { 
+			id_str : '1234',
+			user : {
+				name : '',
+				screen_name : '',
+				profile_image_url_https : ''
+			},
+			text : 'hello',
+			created_at : '2015-01-01'
+		}
+	});
   });
 }
